0e3a4a48821159a4b365ff738c5b7e52e00b16da
omkar <omkar.c.344@gmail.com>
2022-07-12 12:07:51 +0530

    added chainlink aggregator contract

---
+++ contracts/chainlinkAggregator.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.8;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract chainlinkAggregator {
+    function getLatestPrice(address priceFeed) external view returns (int256) {
+        (
+            uint80 roundID,
+            int256 price,
+            uint256 startedAt,
+            uint256 timeStamp,
+            uint80 answeredInRound
+        ) = AggregatorV3Interface(priceFeed).latestRoundData();
+        return price;
+    }
+
+    function decimals(address priceFeed) external view returns (uint8) {
+        return AggregatorV3Interface(priceFeed).decimals();
+    }
+}
