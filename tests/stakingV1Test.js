7476d734cf86efd4a62f055764b6ed11e403fe96
omkar <omkar.c.344@gmail.com>
2022-07-13 14:36:14 +0530

    added more test cases

--- tests/stakingV1Test.js
+++ tests/stakingV1Test.js
@@ -6,13 +6,18 @@ describe("stablecoin staking", () => {
   let owner;
   let addr1;
   let addr2;
+  let dai;
+  let DAI;
+  const daiPricefeed = "0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF";
+  let chainlinkaggregator;
+  let aggregator;
 
   let token;
   let TOKEN;
 
   let STAKING;
   let staking;
-  const DAIPerUSD = "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa";
+  const DAIPerUSD = "0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735";
   const USDCPerUSD = "0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB";
   beforeEach(async () => {
     [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
@@ -22,27 +27,78 @@ describe("stablecoin staking", () => {
     token = await upgrades.deployProxy(TOKEN, { kind: "uups" });
     await token.deployed();
 
+    chainlinkaggregator = await ethers.getContractFactory(
+      "chainlinkAggregator"
+    );
+    aggregator = await chainlinkaggregator.deploy();
+    await aggregator.deployed();
     // Deploy the  contract.
     STAKING = await ethers.getContractFactory("staking");
-    staking = await upgrades.deployProxy(STAKING, [token.address], {
-      kind: "uups",
-    });
+    staking = await upgrades.deployProxy(
+      STAKING,
+      [token.address, aggregator.address],
+      {
+        kind: "uups",
+      }
+    );
     await staking.deployed();
 
     //distribute tokens
     await token.transfer(staking.address, 10000);
     await token.transfer(owner.address, 10000);
-    await token.transfer(addr1.address, 10000);
+
     await token.approve(staking.address, 5000);
     await token.connect(addr1).approve(staking.address, 5000);
 
     await token.approve(owner.address, 5000);
+
+    DAI = await ethers.getContractFactory("Dai");
+    dai = await DAI.attach(DAIPerUSD);
+
+    // gib stakingV1 contract allowance for Dai tokens.
+    dai.connect(addr1).approve(staking.address, 100);
+    dai.connect(addr2).approve(staking.address, 100);
+    //add dai stablecoin support
+    await staking.addNewStableCoin(DAIPerUSD, daiPricefeed, 1);
   });
 
-  it("owner should be able to add & remove stablecoins ", async function () {
-    await staking.addNewStableCoin(DAIPerUSD, 1);
+  it("owner should not be able to add multiple stablecoin for the same id ", async function () {
+    await expect(
+      staking.addNewStableCoin(DAIPerUSD, daiPricefeed, 1)
+    ).to.be.revertedWith(
+      "addNewStableCoin: stablecoin with this Id or address already exists"
+    );
+  });
 
-    await staking.addNewStableCoin(USDCPerUSD, 1);
-    //  await staking.addNewStableCoin(DAI_Contract, 1);
+  it("user should be able to stakecoin ", async function () {
+    await staking.connect(addr1).stakeCoin(1, 100);
+    const contractbalance = await dai.balanceOf(staking.address);
+    //see if the transfer was succesful
+    expect(contractbalance).to.equal(100);
+  });
+
+  it("user should be able to unstake stakecoin ", async function () {
+    await staking.connect(addr1).stakeCoin(1, 100);
+    const balanceBeforeStaking = await token.balanceOf(addr1.address);
+    expect(balanceBeforeStaking).to.equal(0);
+    // stake 100 tokens for 6 months
+    await ethers.provider.send("evm_increaseTime", [2629743 * 6]);
+
+    // cannot withdraw more tokens than staked
+    await expect(staking.connect(addr1).unStakeCoin(1, 150)).to.be.revertedWith(
+      "UnstakeCoin: you do not have enough staked tokens"
+    );
+    const daiBalancebefore = await dai.balanceOf(addr1.address);
+
+    await staking.connect(addr1).unStakeCoin(1, 100);
+    const balanceafterStaking = await token.balanceOf(addr1.address);
+    expect(balanceBeforeStaking).to.not.equal(balanceafterStaking);
+
+    //100 tokens staked for 6 months at 10percent apr should receive 5 tokens
+    expect(balanceafterStaking).to.equal(5);
+    const daiBalanceAfter = await dai.balanceOf(addr1.address);
+    const diff = daiBalanceAfter - daiBalancebefore;
+    // user gets back his staked stablecoin
+    expect(diff).to.equal(100);
   });
 });
