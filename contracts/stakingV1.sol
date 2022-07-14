49cfa6069ab303ba3ac17cf65c7e0bfa67b978f9
omkar <omkar.c.344@gmail.com>
2022-07-14 12:53:06 +0530

    refactored code structure, added natspec comments

--- contracts/stakingV1.sol
+++ contracts/stakingV1.sol
@@ -21,74 +21,42 @@ contract staking is OwnableUpgradeable, UUPSUpgradeable {
 
     address[] public listOfSupportedStableCoinAddress;
 
-    // events
+    event newStaked(
+        address indexed owner,
+        uint256 stablecoinId,
+        uint256 amount
+    );
 
-    //struct
+    event unStaked(
+        address indexed owner,
+        uint256 stablecoinId,
+        uint256 amount,
+        uint256 reward
+    );
+
     struct StablecoinHolder {
-        uint8 stablecoinId;
         uint256 stakingStartTimeStamp;
         uint256 amount;
+        uint256 stablecoinId;
         bool isStaked;
     }
 
     struct Stablecoin {
         address stablecoinAddress;
         address priceFeed;
-        uint8 stablecoinId;
         uint256 listPointer;
+        uint256 stablecoinId;
     }
 
-    //mappings
+    ///@dev maps from stablecoin ID to properties of the stablecoin stored in struct
     mapping(uint256 => Stablecoin) public ListOfStableCoins;
+
+    ///@dev maps from user address to => stablecoinID => properties of user in struct to, double mapping was used so one user can stake diffrent stablecoins
     mapping(address => mapping(uint256 => StablecoinHolder))
         public ListOfStableCoinHolders;
 
+    ///@dev stablecoinIds stored in array
     uint256[] private stablecoinIds;
-
-    function stablecoinExists(address _stablecoinAddress, uint256 _stablecoinId)
-        internal
-        view
-        returns (bool success)
-    {
-        return (ListOfStableCoins[_stablecoinId].stablecoinId ==
-            _stablecoinId &&
-            ListOfStableCoins[_stablecoinId].stablecoinAddress ==
-            _stablecoinAddress);
-    }
-
-    function addNewStableCoin(
-        address _stablecoinAddress,
-        address _priceFeed,
-        uint8 _stablecoinId
-    ) external onlyOwner {
-        require(
-            !stablecoinExists(_stablecoinAddress, _stablecoinId),
-            "addNewStableCoin: stablecoin with this Id or address already exists"
-        );
-
-        ListOfStableCoins[_stablecoinId].stablecoinAddress = _stablecoinAddress;
-        ListOfStableCoins[_stablecoinId].stablecoinId = _stablecoinId;
-        ListOfStableCoins[_stablecoinId].priceFeed = _priceFeed;
-        stablecoinIds.push(_stablecoinId);
-        ListOfStableCoins[_stablecoinId].listPointer = stablecoinIds.length - 1;
-    }
-
-    function removeStablecoin(address _stablecoinAddress, uint256 _stablecoinId)
-        external
-        onlyOwner
-    {
-        require(
-            stablecoinExists(_stablecoinAddress, _stablecoinId),
-            "addNewStableCoin: stablecoin with this Id does not exist"
-        );
-
-        uint256 keyToDelete = ListOfStableCoins[_stablecoinId].listPointer;
-        uint256 keyToMove = stablecoinIds[stablecoinIds.length - 1];
-        stablecoinIds[keyToDelete] = keyToMove;
-        ListOfStableCoins[keyToMove].listPointer = keyToDelete;
-        stablecoinIds.pop();
-        delete ListOfStableCoins[_stablecoinId];
-    }
 
     function initialize(
         IERC20Upgradeable _token,
@@ -107,7 +75,66 @@ contract staking is OwnableUpgradeable, UUPSUpgradeable {
         onlyOwner
     {}
 
-    function stakeCoin(uint8 _stablecoinId, uint256 _amount) external {
+    function stablecoinExists(address _stablecoinAddress, uint256 _stablecoinId)
+        internal
+        view
+        returns (bool success)
+    {
+        if (stablecoinIds.length == 0) return false;
+        return (stablecoinIds[ListOfStableCoins[_stablecoinId].listPointer] ==
+            _stablecoinId &&
+            ListOfStableCoins[_stablecoinId].stablecoinAddress ==
+            _stablecoinAddress);
+    }
+
+    /// @notice lets only owner add new stablecoin support
+    /// @dev location of listpointer can change hence ID of stablecoin was used as key
+    /// @param _stablecoinAddress contract address of the stablecoin to add
+    /// @param _priceFeed chainlink pricefeed of stablecoin/USD pair
+    /// @param _stablecoinId  id of the stablecoin which can be used to access the stablecoin properties
+    function addNewStableCoin(
+        address _stablecoinAddress,
+        address _priceFeed,
+        uint256 _stablecoinId
+    ) external onlyOwner {
+        require(
+            !stablecoinExists(_stablecoinAddress, _stablecoinId),
+            "addNewStableCoin: stablecoin with this Id or address already exists"
+        );
+
+        ListOfStableCoins[_stablecoinId].stablecoinAddress = _stablecoinAddress;
+        ListOfStableCoins[_stablecoinId].stablecoinId = _stablecoinId;
+        ListOfStableCoins[_stablecoinId].priceFeed = _priceFeed;
+        stablecoinIds.push(_stablecoinId);
+        ListOfStableCoins[_stablecoinId].listPointer = stablecoinIds.length - 1;
+    }
+
+    /// @notice lets owner remove stablecoin support
+    /// @dev deadline parameter could be hardcoded in contract
+    /// @param _stablecoinAddress contract address of the stablecoin to add
+    /// @param _stablecoinId  id of the stablecoin which can be used to access the stablecoin properties
+    function removeStablecoin(address _stablecoinAddress, uint256 _stablecoinId)
+        external
+        onlyOwner
+    {
+        require(
+            stablecoinExists(_stablecoinAddress, _stablecoinId),
+            "addNewStableCoin: stablecoin with this Id does not exist"
+        );
+
+        uint256 keyToDelete = ListOfStableCoins[_stablecoinId].listPointer;
+        uint256 keyToMove = stablecoinIds[stablecoinIds.length - 1];
+        stablecoinIds[keyToDelete] = keyToMove;
+        ListOfStableCoins[keyToMove].listPointer = uint256(keyToDelete);
+        stablecoinIds.pop();
+        delete ListOfStableCoins[_stablecoinId];
+    }
+
+    /// @notice lets users stake their stablecoins
+    /// @dev deadline parameter could be hardcoded in contract
+    /// @param _stablecoinId  id of the stablecoin which can be used to access the stablecoin properties
+    /// @param _amount the amount of stablecoin user wishes to stake
+    function stakeCoin(uint256 _stablecoinId, uint256 _amount) external {
         address stablecoinAddress = ListOfStableCoins[_stablecoinId]
             .stablecoinAddress;
 
@@ -115,7 +142,10 @@ contract staking is OwnableUpgradeable, UUPSUpgradeable {
         StablecoinHolder storage stablecoinholder = ListOfStableCoinHolders[
             msgSender
         ][_stablecoinId];
-
+        require(
+            stablecoinExists(stablecoinAddress, _stablecoinId),
+            "addNewStableCoin: stablecoin with this Id does not exist"
+        );
         require(
             stablecoinholder.isStaked == false,
             "stakeCoin: you have already staked your tokens, unstake your tokens to stake new tokens"
@@ -123,12 +153,11 @@ contract staking is OwnableUpgradeable, UUPSUpgradeable {
         require(
             IERC20Upgradeable(stablecoinAddress).balanceOf(msgSender) > _amount
         );
-        // require stablecoin exists
 
         ListOfStableCoinHolders[msgSender][_stablecoinId] = StablecoinHolder(
-            _stablecoinId,
             block.timestamp,
             _amount,
+            _stablecoinId,
             true
         );
 
@@ -137,15 +166,24 @@ contract staking is OwnableUpgradeable, UUPSUpgradeable {
             address(this),
             _amount
         );
+        emit newStaked(msgSender, _stablecoinId, _amount);
     }
 
-    function unStakeCoin(uint8 _stablecoinId, uint256 _amount) external {
+    /// @notice lets users unstake their stablecoins
+    /// @dev deadline parameter could be hardcoded in contract
+    /// @param _stablecoinId  id of the stablecoin which can be used to access the stablecoin properties
+    /// @param _amount the amount of stablecoin user wishes to unstake
+    function unStakeCoin(uint256 _stablecoinId, uint256 _amount) external {
         address stablecoinAddress = ListOfStableCoins[_stablecoinId]
             .stablecoinAddress;
         address msgSender = msg.sender;
         StablecoinHolder storage stablecoinholder = ListOfStableCoinHolders[
             msgSender
         ][_stablecoinId];
+        require(
+            stablecoinExists(stablecoinAddress, _stablecoinId),
+            "addNewStableCoin: stablecoin with this Id does not exist"
+        );
 
         require(
             stablecoinholder.isStaked == true,
@@ -155,7 +193,6 @@ contract staking is OwnableUpgradeable, UUPSUpgradeable {
             stablecoinholder.amount >= _amount,
             "UnstakeCoin: you do not have enough staked tokens"
         );
-        // require stablecoin exists
 
         uint256 timeElapsed = calculateTimeElapsed(
             stablecoinholder.stakingStartTimeStamp
@@ -164,6 +201,7 @@ contract staking is OwnableUpgradeable, UUPSUpgradeable {
 
         uint256 reward = getReward(_amount, timeElapsed, priceFeed);
 
+        stablecoinholder.isStaked = false;
         // transfer the staked stablecoin
         IERC20Upgradeable(stablecoinAddress).safeTransferFrom(
             address(this),
@@ -173,8 +211,12 @@ contract staking is OwnableUpgradeable, UUPSUpgradeable {
 
         // transfer reward accumulated
         token.safeTransfer(msgSender, reward);
+        emit unStaked(msgSender, _stablecoinId, _amount, reward);
     }
 
+    /// @notice lets users unstake their stablecoins
+    /// @param stakingStartTimeStamp  id of the stablecoin which can be used to access the stablecoin properties
+    /// @return timeElapsed returns the amount of the for which tokens were staked
     function calculateTimeElapsed(uint256 stakingStartTimeStamp)
         internal
         view
@@ -183,28 +225,44 @@ contract staking is OwnableUpgradeable, UUPSUpgradeable {
         return block.timestamp - stakingStartTimeStamp;
     }
 
+    /// @notice function calculates the perks in terms of percentage based on USD valo of tokens
+    /// @dev chainlink price agregator was used to get USD value of stablecoins
+    /// @param priceFeed chainlink pricefeed of stablecoin/USD pair
+    /// @param _amount the amount of stablecoin user wishes to unstake
+    /// @return "perks" returns the perks in terms of percentage
     function getPerks(address priceFeed, uint256 _amount)
         internal
         view
         returns (uint256)
     {
-        
         uint256 priceOfStablecoin = uint256(
             aggregator.getLatestPrice(priceFeed)
         );
-        uint256 usdValueOfStablecoins = priceOfStablecoin * _amount;
+        uint256 decimalsDenominator = uint256(
+            10**aggregator.decimals(priceFeed)
+        );
+        uint256 usdValueOfStablecoins = (priceOfStablecoin * _amount) /
+            decimalsDenominator;
+        console.log(usdValueOfStablecoins, "usdValueOfStablecoins");
         if (usdValueOfStablecoins < 100) return 0;
         else if (usdValueOfStablecoins < 500) return 2;
         else if (usdValueOfStablecoins < 1000) return 5;
         else return 10;
     }
 
+    /// @notice function calculates the perks in terms of percentage based on USD valo of tokens
+    /// @dev days parameter was used , leap year was not taken into consideration
+    /// @param timeElapsed amount of time tokens staked
+    /// @param priceFeed chainlink pricefeed of stablecoin/USD pair
+    /// @param amount the amount of stablecoin user wishes to unstake
+    /// @return "perks & interest rate" returns the interest rate + perks in terms of percentage
     function getinterestRateAndPerks(
         uint256 timeElapsed,
         address priceFeed,
         uint256 amount
     ) internal view returns (uint256) {
         uint256 perks = getPerks(priceFeed, amount);
+        console.log(perks, "perks");
         if (timeElapsed < 31 days) {
             return 5 + perks;
         } else if (timeElapsed < 183 days) {
@@ -214,6 +272,12 @@ contract staking is OwnableUpgradeable, UUPSUpgradeable {
         } else return 18 + perks;
     }
 
+    /// @notice function calculates the rewards for user based on interest & perks
+    /// @dev days parameter was used , 1 year = 365 days
+    /// @param priceFeed chainlink pricefeed of stablecoin/USD pair
+    /// @param timeElapsed amount of time tokens staked
+    /// @param amount the amount of stablecoin user wishes to unstake
+    /// @return "reward" returns the rewards to send for user
     function getReward(
         uint256 amount,
         uint256 timeElapsed,
@@ -224,6 +288,8 @@ contract staking is OwnableUpgradeable, UUPSUpgradeable {
             priceFeed,
             amount
         );
+        console.log(interestAndPerk, "interest plus perks");
+
         uint256 denominator = 365 days * 100;
         uint256 reward = (interestAndPerk * amount * timeElapsed) / denominator;
         return reward;
